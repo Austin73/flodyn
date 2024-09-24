894c5a115b468008b8c2d8ebbf34a7f45f0bb24e
Maneesh Kumar Pal <86200227+Austin73@users.noreply.github.com>
2024-09-24 21:23:53 +0530

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,45 +1,54 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig( {
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: false },
-  ssr: false,
-  modules: [
-    'vuetify-nuxt-module'
-  ],
-  features: {
-    inlineStyles: false
-  },
-  css: ['assets/styles/main.scss'],
-  vuetify: {
-    moduleOptions: {      
+export default defineNuxtConfig({
+  app: {
+    head: {
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Lato:wght@400;700&display=swap",
+        },
+      ],
     },
+  },
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  ssr: false,
+  modules: ["vuetify-nuxt-module"],
+  features: {
+    inlineStyles: false,
+  },
+
+  css: ["assets/styles/main.scss"],
+
+  vuetify: {
+    moduleOptions: {},
     vuetifyOptions: {
       defaults: {
         VBtn: {
-          class: 'text-none'
+          class: "text-none",
         },
         VSheet: {
-          color: 'transparent',
-        }
+          color: "transparent",
+        },
       },
       theme: {
         themes: {
           light: {
             dark: false,
             colors: {
-              primary: '#1F299C',
-              secondary: '#2ABC68',
-            }
+              primary: "#1F299C",
+              secondary: "#2ABC68",
+            },
           },
           dark: {
             dark: true,
             colors: {
-              primary: '#1F299C',
-              secondary: '#2ABC68',
-            }
-          }
-        }
-      }
-    }
+              primary: "#1F299C",
+              secondary: "#2ABC68",
+            },
+          },
+        },
+      },
+    },
   },
-} )
+});
