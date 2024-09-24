ce67d5507a404466458cbff46bc14229e24a3f89
Maneesh Kumar Pal <86200227+Austin73@users.noreply.github.com>
2024-09-24 21:21:58 +0530

    Update contact.vue

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,5 +1,5 @@
 <template>
-  <FlodyneContainer color="primary">
+  <FlodyneContainer color="primary" class="font-inter">
     <template #title>Contact <span class="text-secondary">Us</span></template>
     <template #subtitle>To know more about us please fill this form</template>
 
@@ -17,15 +17,27 @@
           <VTextField label="Email" variant="outlined" v-model="email" />
         </VCol>
         <VCol cols="6">
-          <VTextField label="Company Name" variant="outlined" v-model="company" />
+          <VTextField
+            label="Company Name"
+            variant="outlined"
+            v-model="company"
+          />
         </VCol>
         <VCol cols="6">
-          <VTextField label="Product Name" variant="outlined" v-model="product" />
+          <VTextField
+            label="Product Name"
+            variant="outlined"
+            v-model="product"
+          />
         </VCol>
       </VRow>
       <VRow>
         <VCol>
-          <VTextarea label="Description" variant="outlined" v-model="description" />
+          <VTextarea
+            label="Description"
+            variant="outlined"
+            v-model="description"
+          />
         </VCol>
       </VRow>
       <VRow justify="center">
@@ -40,21 +52,23 @@
     <VSheet height="500" rounded="xl">
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9315.968555995054!2d-73.766153459043!3d42.65401377088924!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89de0a37fb28709d%3A0xdf2d1d8ef3ead42!2sWashington%20Park!5e0!3m2!1svi!2sus!4v1721690425854!5m2!1svi!2sus"
-        width="100%" height="100%" style="border:0;" loading="lazy"
-        referrerpolicy="no-referrer-when-downgrade"></iframe>
+        width="100%"
+        height="100%"
+        style="border: 0"
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"
+      ></iframe>
     </VSheet>
   </FlodyneContainer>
-
 </template>
 
 <script lang="ts" setup>
-  const name = ref('')
-  const phone = ref('')
-  const email = ref('')
-  const company = ref('')
-  const product = ref('')
-  const description = ref('')
-
+const name = ref("");
+const phone = ref("");
+const email = ref("");
+const company = ref("");
+const product = ref("");
+const description = ref("");
 </script>
 
 <style></style>
