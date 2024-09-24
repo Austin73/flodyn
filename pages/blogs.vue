e7b11ac4283c8dd2f7ab4ced954b7aa439388021
Maneesh Kumar Pal <86200227+Austin73@users.noreply.github.com>
2024-09-24 21:21:29 +0530

    Update blogs.vue

--- pages/blogs.vue
+++ pages/blogs.vue
@@ -1,24 +1,30 @@
 <template>
   <FlodyneContainer>
-
     <VRow>
-      <VCol cols="12" md="4" v-for="i in 6" :key="i">
+      <VCol cols="12" md="4" v-for="i in 6" :key="i" class="font-lato">
         <VCard rounded="lg" to="/" variant="outlined" border="grey thin">
-          <VImg :aspect-ratio="16 / 9" cover src="https://picsum.photos/400/300" alt="Card Image" />
+          <VImg
+            :aspect-ratio="16 / 9"
+            cover
+            src="/images/product-content.svg"
+            alt="Card Image"
+          />
           <VRow no-gutters align="center">
             <VCol>
-              <VCardTitle class="text-secondary">Aerospace</VCardTitle>
+              <VCardTitle class="text-secondary">Flodyne is on air</VCardTitle>
             </VCol>
             <VCol cols="auto">
-              <div class="text-caption text-primary mr-3">Fri, 24 june 2024</div>
+              <div class="text-caption text-primary mr-3">
+                Fri, 24 june 2024
+              </div>
             </VCol>
           </VRow>
           <VCardText>
-            We have excellent Machining, Fabrication & Testing Facilities, housed at our works, which enables us to
-            manufacture most steel components ourselves thereby ensuring full control on quality and deliveries
-            which
-            are
-            very essential in meeting customer requirements of Total Satisfaction.
+            We have excellent Machining, Fabrication & Testing Facilities,
+            housed at our works, which enables us to manufacture most steel
+            components ourselves thereby ensuring full control on quality and
+            deliveries which are very essential in meeting customer requirements
+            of Total Satisfaction.
           </VCardText>
         </VCard>
       </VCol>
@@ -26,8 +32,6 @@
   </FlodyneContainer>
 </template>
 
-<script lang="ts" setup>
-
-</script>
+<script lang="ts" setup></script>
 
 <style></style>
