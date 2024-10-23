b70bbc3b0486132219896fdc71ae2afe81b21cea
Maneesh Kumar Pal <86200227+Austin73@users.noreply.github.com>
2024-10-24 00:27:03 +0530

    Update navigation-drawer.vue

--- components/flodyne/navigation-drawer.vue
+++ components/flodyne/navigation-drawer.vue
@@ -1,13 +1,32 @@
 <template>
   <VNavigationDrawer v-model="drawer" location="right">
-    <VListItem nav v-for="link in links" :key="link.title" :to="link.to" :title="link.title" />
+    <VListItem
+      nav
+      v-for="link in links"
+      :key="link.title"
+      :to="link.to"
+      :title="link.title"
+      class="reduced-spacing bg"
+    >
+      <template #title>
+        <span class="title-spacing">{{ link.title }}</span>
+      </template>
+    </VListItem>
   </VNavigationDrawer>
 </template>
 
 <script lang="ts" setup>
-  const drawer = useState<boolean>('drawer')
+const drawer = useState<boolean>("drawer");
 
-  const links = useState<{ title: string, to: string }[]>('links')
+const links = useState<{ title: string; to: string }[]>("links");
 </script>
 
-<style></style>
+<style scoped>
+.reduced-spacing {
+  letter-spacing: normal;
+}
+
+.title-spacing {
+  letter-spacing: -0.5px; /* Adjust this value as needed */
+}
+</style>
