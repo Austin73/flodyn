0dedff21405943fe1a9e157bf014764ae53d3fd9
Maneesh Kumar Pal <86200227+Austin73@users.noreply.github.com>
2024-10-24 00:25:19 +0530

    Update products.vue

--- pages/products.vue
+++ pages/products.vue
@@ -18,6 +18,7 @@
                 cover
                 src="/images/product-content.svg"
                 alt="Card Image"
+                class="zoom-on-hover"
               />
               <VCardTitle class="text-secondary">Aerospace</VCardTitle>
               <VCardText>
@@ -36,6 +37,7 @@
                 cover
                 src="/images/product-content.svg"
                 alt="Card Image"
+                class="zoom-on-hover"
               />
               <VCardTitle class="text-secondary">Oil & Gas</VCardTitle>
               <VCardText>
@@ -54,6 +56,7 @@
                 cover
                 src="/images/product-content.svg"
                 alt="Card Image"
+                class="zoom-on-hover"
               />
               <VCardTitle class="text-secondary">Automotive</VCardTitle>
               <VCardText>
@@ -90,17 +93,43 @@
 
   <FlodyneContainer>
     <template #title>Our <span class="text-secondary">Products</span></template>
-    <template #subtitle>We are working in lost of product category</template>
+    <template #subtitle>Explore our core product & service offerings</template>
 
     <VRow>
       <VCol cols="12" md="4">
         <VListSubheader class="text-primary font-weight-bold"
           >Our Categories</VListSubheader
         >
+        <VListItem title="Steel" class="bg-green-lighten-5 pr-0 py-0 mb-2">
+          <template #append>
+            <VBtn color="secondary" variant="flat" width="60">
+              <VIcon>mdi-arrow-right</VIcon>
+            </VBtn>
+          </template>
+        </VListItem>
+        <VListItem title="Power" class="bg-green-lighten-5 pr-0 py-0 mb-2">
+          <template #append>
+            <VBtn color="secondary" variant="flat" width="60">
+              <VIcon>mdi-arrow-right</VIcon>
+            </VBtn>
+          </template>
+        </VListItem>
+        <VListItem title="Aerospace" class="bg-green-lighten-5 pr-0 py-0 mb-2">
+          <template #append>
+            <VBtn color="secondary" variant="flat" width="60">
+              <VIcon>mdi-arrow-right</VIcon>
+            </VBtn>
+          </template>
+        </VListItem>
+        <VListItem title="Mining" class="bg-green-lighten-5 pr-0 py-0 mb-2">
+          <template #append>
+            <VBtn color="secondary" variant="flat" width="60">
+              <VIcon>mdi-arrow-right</VIcon>
+            </VBtn>
+          </template>
+        </VListItem>
         <VListItem
-          v-for="i in 10"
-          :key="i"
-          title="Straight Series"
+          title="Construction"
           class="bg-green-lighten-5 pr-0 py-0 mb-2"
         >
           <template #append>
@@ -112,15 +141,193 @@
       </VCol>
       <VCol cols="12" md="8">
         <VRow>
-          <VCol cols="12" md="4" v-for="i in 6" :key="i">
+          <VCol cols="12" md="4">
             <VCard to="/" class="mb-4 text-center">
               <VImg
                 :aspect-ratio="4 / 3"
                 cover
-                src="/images/our-products.png"
+                src="/images/products/4Hydraulic Cylinder.jpg"
                 alt="Card Image"
+                class="zoom-on-hover"
               />
-              <VCardTitle class="text-body-1">Flo dyne Machine</VCardTitle>
+              <VCardTitle class="text-body-1">Hydraulic Cylinder</VCardTitle>
+              <VCardActions class="justify-center">
+                <VBtn color="primary" variant="flat" class="px-5"
+                  >Read More</VBtn
+                >
+              </VCardActions>
+            </VCard>
+          </VCol>
+          <VCol cols="12" md="4">
+            <VCard to="/" class="mb-4 text-center">
+              <VImg
+                :aspect-ratio="4 / 3"
+                cover
+                src="/images/products/5Filtration Trolley.jpg"
+                alt="Card Image"
+                class="zoom-on-hover"
+              />
+              <VCardTitle class="text-body-1">Filtration Trolley</VCardTitle>
+              <VCardActions class="justify-center">
+                <VBtn color="primary" variant="flat" class="px-5"
+                  >Read More</VBtn
+                >
+              </VCardActions>
+            </VCard>
+          </VCol>
+          <VCol cols="12" md="4">
+            <VCard to="/" class="mb-4 text-center">
+              <VImg
+                :aspect-ratio="4 / 3"
+                cover
+                src="/images/products/6Hydraulic Pump.jpg"
+                alt="Card Image"
+                class="zoom-on-hover"
+              />
+              <VCardTitle class="text-body-1">Hydraulic Pump</VCardTitle>
+              <VCardActions class="justify-center">
+                <VBtn color="primary" variant="flat" class="px-5"
+                  >Read More</VBtn
+                >
+              </VCardActions>
+            </VCard>
+          </VCol>
+          <VCol cols="12" md="4">
+            <VCard to="/" class="mb-4 text-center">
+              <VImg
+                :aspect-ratio="4 / 3"
+                cover
+                src="/images/products/7Hydraulic Motor.jpg"
+                alt="Card Image"
+                class="zoom-on-hover"
+              />
+              <VCardTitle class="text-body-1">Hydraulic Motor</VCardTitle>
+              <VCardActions class="justify-center">
+                <VBtn color="primary" variant="flat" class="px-5"
+                  >Read More</VBtn
+                >
+              </VCardActions>
+            </VCard>
+          </VCol>
+          <VCol cols="12" md="4">
+            <VCard to="/" class="mb-4 text-center">
+              <VImg
+                :aspect-ratio="4 / 3"
+                cover
+                src="/images/products/8Hydraulic Valves.jpg"
+                alt="Card Image"
+                class="zoom-on-hover"
+              />
+              <VCardTitle class="text-body-1">Hydraulic Valves</VCardTitle>
+              <VCardActions class="justify-center">
+                <VBtn color="primary" variant="flat" class="px-5"
+                  >Read More</VBtn
+                >
+              </VCardActions>
+            </VCard>
+          </VCol>
+          <VCol cols="12" md="4">
+            <VCard to="/" class="mb-4 text-center">
+              <VImg
+                :aspect-ratio="4 / 3"
+                cover
+                src="/images/products/9.Mobile Valves.jpg"
+                alt="Card Image"
+                class="zoom-on-hover"
+              />
+              <VCardTitle class="text-body-1">Mobile Valves</VCardTitle>
+              <VCardActions class="justify-center">
+                <VBtn color="primary" variant="flat" class="px-5"
+                  >Read More</VBtn
+                >
+              </VCardActions>
+            </VCard>
+          </VCol>
+
+          <VCol cols="12" md="4">
+            <VCard to="/" class="mb-4 text-center">
+              <VImg
+                :aspect-ratio="4 / 3"
+                cover
+                src="/images/products/10Ball Valve.jpg"
+                alt="Card Image"
+                class="zoom-on-hover"
+              />
+              <VCardTitle class="text-body-1">Ball Valve</VCardTitle>
+              <VCardActions class="justify-center">
+                <VBtn color="primary" variant="flat" class="px-5"
+                  >Read More</VBtn
+                >
+              </VCardActions>
+            </VCard>
+          </VCol>
+
+          <VCol cols="12" md="4">
+            <VCard to="/" class="mb-4 text-center">
+              <VImg
+                :aspect-ratio="4 / 3"
+                cover
+                src="/images/products/17Valve stand.jpg"
+                alt="Card Image"
+                class="zoom-on-hover"
+              />
+              <VCardTitle class="text-body-1">Valve stand</VCardTitle>
+              <VCardActions class="justify-center">
+                <VBtn color="primary" variant="flat" class="px-5"
+                  >Read More</VBtn
+                >
+              </VCardActions>
+            </VCard>
+          </VCol>
+
+          <VCol cols="12" md="4">
+            <VCard to="/" class="mb-4 text-center">
+              <VImg
+                :aspect-ratio="4 / 3"
+                cover
+                src="/images/products/18.SAE Flanges.jpg"
+                alt="Card Image"
+                class="zoom-on-hover"
+              />
+              <VCardTitle class="text-body-1">SAE Flanges</VCardTitle>
+              <VCardActions class="justify-center">
+                <VBtn color="primary" variant="flat" class="px-5"
+                  >Read More</VBtn
+                >
+              </VCardActions>
+            </VCard>
+          </VCol>
+
+          <VCol cols="12" md="4">
+            <VCard to="/" class="mb-4 text-center">
+              <VImg
+                :aspect-ratio="4 / 3"
+                cover
+                src="/images/products/19Quick Release Couplings.jpg"
+                alt="Card Image"
+                class="zoom-on-hover"
+              />
+              <VCardTitle class="text-body-1"
+                >Quick Release Couplings</VCardTitle
+              >
+              <VCardActions class="justify-center">
+                <VBtn color="primary" variant="flat" class="px-5"
+                  >Read More</VBtn
+                >
+              </VCardActions>
+            </VCard>
+          </VCol>
+
+          <VCol cols="12" md="4">
+            <VCard to="/" class="mb-4 text-center">
+              <VImg
+                :aspect-ratio="4 / 3"
+                cover
+                src="/images/products/HPU.jpg"
+                alt="Card Image"
+                class="zoom-on-hover"
+              />
+              <VCardTitle class="text-body-1">HPU</VCardTitle>
               <VCardActions class="justify-center">
                 <VBtn color="primary" variant="flat" class="px-5"
                   >Read More</VBtn
@@ -132,6 +339,14 @@
       </VCol>
     </VRow>
   </FlodyneContainer>
+  <VBtn
+    class="back-to-top"
+    color="secondary"
+    @click="scrollToTop"
+    rounded="pill"
+  >
+    Back to Top
+  </VBtn>
 </template>
 
 <script lang="ts" setup>
@@ -148,4 +363,19 @@ function prev() {
 }
 </script>
 
-<style></style>
+<style scoped>
+.zoom-on-hover {
+  transition: transform 0.5s ease; /* Smooth transition for the zoom effect */
+}
+
+.zoom-on-hover:hover {
+  transform: scale(1.2); /* Zoom in by 10% on hover */
+}
+
+.back-to-top {
+  position: fixed;
+  bottom: 20px; /* Distance from the bottom of the page */
+  right: 20px; /* Distance from the right of the page */
+  z-index: 1000; /* Ensures it's always on top of other elements */
+}
+</style>
